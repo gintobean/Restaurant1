e769c2d5a11164513c08978753cf9b20afec8a3d
gintobean <87101188+gintobean@users.noreply.github.com>
2021-08-11 13:19:58 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -43,12 +43,26 @@ gsap.from(".veg", {
 gsap.from(".bg2", {
   scrollTrigger:
   {
-    trigger: ".bg2",
-    start:"#testemonials",
+    trigger: "#menu",
+    start:"bottom bottom",
+    endTrigger: ".bg2",
+    end: "bottom top",
     scrub: true
   },
-  y:-674,
-  duration:1
+  y:-400,
+  duration:3
+});
+
+gsap.from("#testemonials", {
+  scrollTrigger:
+  {
+    trigger: "#testemonials",
+    start:"top bottom",
+    end: "bottom bottom",
+    scrub: true
+  },
+  y:-400,
+  duration:3
 });
 
 gsap.from(".title2, .testemonial-body",{
@@ -64,12 +78,14 @@ gsap.from(".title2, .testemonial-body",{
 gsap.from(".decor", {
   scrollTrigger:
   {
-    trigger: ".decor",
-    start:"#footer",
+    trigger: "#testemonials",
+    start:"bottom bottom",
+    endTrigger:"#footer",
+    end: "bottom bottom",
     scrub: true
   },
-  y:-938,
-  duration:1
+  y:-400,
+  duration:2
 });
 
 gsap.from(".footer-box",{
@@ -82,6 +98,37 @@ gsap.from(".footer-box",{
   duration:2
 });
 
+gsap.from("#footer",{
+  scrollTrigger:
+  {
+    trigger: "#footer",
+    start: "top bottom",
+    scrub: true
+  },
+  y:-400,
+  duration:3
+});
+
+//Review Timeline
+let tl = gsap.timeline({scrollTrigger: {
+  trigger: ".yelp",
+  toggleActions: "restart pause resume pause"
+}});
+tl.from(".review1", {
+  opacity:0,
+  x:-100,
+  duration:1
+}, "-=0.5")
+.from(".review2", {
+  opacity:0,
+  x:100,
+  duration:1
+}, "-=0.5")
+.from(".review3", {
+  opacity:0,
+  x:-100,
+  duration:1
+}, "-=0.5");
 
 // Image hovering
 
