7d9599ab4fd747c1c4b9cfb424b5c19de0c09f22
gintobean <87101188+gintobean@users.noreply.github.com>
2021-08-13 16:55:45 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 //media queries
 if ($(window).width() < 700) {
-    $('.navbar-brand, .nav-item').addClass('mx-auto');
-}
-else {
-    $('.navbar-brand, .nav-item').removeClass('mx-auto');
+  $('.navbar-brand, .nav-item').addClass('mx-auto');
+} else {
+  $('.navbar-brand, .nav-item').removeClass('mx-auto');
 }
 
 // var scrollAmount = Math.floor($(this).scrollTop() / 934);
@@ -21,10 +20,19 @@ else {
 
 //Starting animations
 
-gsap.from('.navbar', {opacity:0,duration:1});
-gsap.from('.bg', {opacity:0,duration:1});
-gsap.from('.title-content', {opacity:0,duration:1,y:-50});
-gsap.from('.menu-cont', {duration:1,y:50});
+gsap.from('.navbar', {
+  opacity: 0,
+  duration: 1
+});
+gsap.from('.bg', {
+  opacity: 0,
+  duration: 1
+});
+gsap.from('.title-content', {
+  opacity: 0,
+  duration: 1,
+  y: -50
+});
 
 //scrolling animations
 gsap.to(".bg", {
@@ -36,117 +44,160 @@ gsap.to(".bg", {
   visibility: 'hidden'
 });
 
-gsap.from(".section-title, .name",{
-  scrollTrigger:
-  {
+gsap.from("#menu-title, .name", {
+  scrollTrigger: {
     trigger: "#menu",
     toggleActions: "restart pause resume pause"
   },
-  opacity:0,
-  duration:2
+  opacity: 0,
+  y: 50,
+  duration: 2
+});
+
+gsap.from(".hours-container", {
+  scrollTrigger: {
+    trigger: "#hours",
+    toggleActions: "restart pause resume pause"
+  },
+  opacity: 0,
+  y: 50,
+  duration: 2
 });
 
 gsap.from(".meat", {
-  scrollTrigger:
-  {
+  scrollTrigger: {
     trigger: ".meat",
     toggleActions: "restart pause resume pause"
   },
-  opacity:0,
-  x:-100,
-  duration:1
+  opacity: 0,
+  x: -100,
+  duration: 1
 });
 
 gsap.from(".veg", {
-  scrollTrigger:
-  {
+  scrollTrigger: {
     trigger: ".veg",
     toggleActions: "restart pause resume pause"
   },
-  opacity:0,
-  x:100,
-  duration:1
+  opacity: 0,
+  x: 100,
+  duration: 1
 });
 
-$(".bg2-wrapper").css('transform','translate(0,25vh)');
-gsap.from(".bg2-wrapper", {ease:"none",
-  scrollTrigger:
-  {
+gsap.from(".restaurant-photo", {
+  scrollTrigger: {
+    trigger: ".restaurant-photo",
+    toggleActions: "restart pause resume pause"
+  },
+  opacity: 0,
+  x: 100,
+  duration: 1
+});
+
+gsap.from(".bg2-wrapper", {
+  ease: "none",
+  scrollTrigger: {
     trigger: "#menu",
-    start:"bottom bottom",
+    start: "bottom bottom",
+    endTrigger: "#hours",
+    end: "top top",
+    scrub: true
+  },
+  y: "-50vh"
+});
+
+gsap.from(".bg3-wrapper", {
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#hours",
+    start: "bottom bottom",
     endTrigger: "#testemonials",
     end: "top top",
     scrub: true
   },
-  y:"-50vh"
+  y: "-50vh"
 });
 
-gsap.from(".title2, .testemonial-body",{
-  scrollTrigger:
-  {
+
+gsap.from(".title2, .testemonial-body", {
+  scrollTrigger: {
     trigger: "#testemonials",
     toggleActions: "restart pause resume pause"
   },
-  opacity:0,
-  duration:2
+  opacity: 0,
+  y: 50,
+  duration: 2
 });
 
-gsap.from(".decor-wrapper", {ease: "none",
-  scrollTrigger:
-  {
+gsap.from(".decor-wrapper", {
+  ease: "none",
+  scrollTrigger: {
 
     trigger: "#testemonials",
-    start:"bottom bottom",
-    endTrigger:"#footer",
+    start: "bottom bottom",
+    endTrigger: "#footer",
     end: "bottom bottom",
     scrub: true,
   },
-  y:"-50vh"
+  y: "-50vh"
 });
 
-gsap.from(".footer-box",{
-  scrollTrigger:
-  {
+gsap.from(".footer-box", {
+  scrollTrigger: {
     trigger: "#footer",
     toggleActions: "restart pause resume pause"
   },
-  opacity:0,
-  duration:2
+  opacity: 0,
+  duration: 2
 });
 
 //Review Timeline
-let tl = gsap.timeline({scrollTrigger: {
-  trigger: ".yelp",
-  toggleActions: "restart pause resume pause"
-}});
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".yelp",
+    toggleActions: "restart pause resume pause"
+  }
+});
 tl.from(".review1", {
-  opacity:0,
-  x:-100,
-  duration:1
-}, "-=0.5")
-.from(".review2", {
-  opacity:0,
-  x:100,
-  duration:1
-}, "-=0.5")
-.from(".review3", {
-  opacity:0,
-  x:-100,
-  duration:1
-}, "-=0.5");
+    opacity: 0,
+    x: -100,
+    duration: 1
+  }, "-=0.5")
+  .from(".review2", {
+    opacity: 0,
+    x: 100,
+    duration: 1
+  }, "-=0.5")
+  .from(".review3", {
+    opacity: 0,
+    x: -100,
+    duration: 1
+  }, "-=0.5");
 
 // Image hovering
 
-$(".meat").on("mouseover",function () {
-  gsap.to('.meat', {duration:0.5,scale:1.1});
+$(".meat").on("mouseover", function() {
+  gsap.to('.meat', {
+    duration: 0.5,
+    scale: 1.1
+  });
 });
-$(".meat").on("mouseout",function () {
-  gsap.to('.meat', {duration:0.5,scale:1});
+$(".meat").on("mouseout", function() {
+  gsap.to('.meat', {
+    duration: 0.5,
+    scale: 1
+  });
 });
 
-$(".veg").on("mouseover",function () {
-  gsap.to('.veg', {duration:0.5,scale:1.1});
+$(".veg").on("mouseover", function() {
+  gsap.to('.veg', {
+    duration: 0.5,
+    scale: 1.1
+  });
 });
-$(".veg").on("mouseout",function () {
-  gsap.to('.veg', {duration:0.5,scale:1});
+$(".veg").on("mouseout", function() {
+  gsap.to('.veg', {
+    duration: 0.5,
+    scale: 1
+  });
 });
