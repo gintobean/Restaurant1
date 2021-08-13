7b06028668086556be0c27b562b50c23ed8bb141
gintobean <87101188+gintobean@users.noreply.github.com>
2021-08-12 17:19:48 -0700

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,1 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
 gsap.from('.navbar', {opacity:0,duration:1});
+gsap.from('.meat-items, .veg-items', {opacity:0,duration:1});
+
+let tl = gsap.timeline({scrollTrigger: {
+  trigger: "#meat-menu",
+  toggleActions: "restart pause resume restart"
+}})
+
+for (var i = 1; i <=4; i++) {
+  tl.from(".pic" + i, {opacity:0, duration:3}, "-=2.5");
+}
+
+let tl2 = gsap.timeline({scrollTrigger: {
+  trigger: "#veg-menu",
+  toggleActions: "restart pause resume restart"
+}})
+
+for (var i = 5; i <=8; i++) {
+  tl2.from(".pic" + i, {opacity:0, duration:3}, "-=2.5");
+}
